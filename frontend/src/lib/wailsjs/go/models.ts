8fdfcb6332951b5e7a97d79a448744ba7adf32d1
Dmitrii Lipin <dimich3d@ya.ru>
2025-03-06 22:40:53 +0100

    ai rampage

---
+++ frontend/src/lib/wailsjs/go/models.ts
@@ -0,0 +1,17 @@
+export namespace main {
+	
+	export class Command {
+	    Name: string;
+	
+	    static createFrom(source: any = {}) {
+	        return new Command(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.Name = source["Name"];
+	    }
+	}
+
+}
+
