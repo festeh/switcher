934ed35a673d7cb239aca8ed82479c8e5dc96b5f
Dmitrii Lipin <dimich3d@ya.ru>
2025-03-15 22:47:52 +0100

    ts

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
   import { onMount } from 'svelte';
   import { GetCommandList } from '../lib/wailsjs/go/main/App';
   import CommandItem from '../lib/CommandItem.svelte';
